b2779f10f3907ba1192364fba7faf1edad691900
Michael Cowley <hybrdthry911@gmail.com>
2014-10-15 15:44:43 -0400

    includesfixed

---
+++ ELPaymentHeader.h
@@ -0,0 +1,54 @@
+//
+//  ELPaymentHeader.h
+//  OrderManager
+//
+//  Created by Mike on 10/15/14.
+//  Copyright (c) 2014 E-Nough Logic. All rights reserved.
+//
+#ifndef OrderManager_ELPaymentHeader_h
+#define OrderManager_ELPaymentHeader_h
+#import "ELView.h"
+#import "ELViewController.h"
+
+#import "ELStripeHeader.h"
+#import "ELAmountTableViewCell.h"
+#import "ELCategory.h"
+#import "ELChangePasswordViewController.h"
+#import "ELCompleteSummaryView.h"
+#import "ELCustomerAccountSettingsViewController.h"
+#import "ELCustomerAccountViewController.h"
+#import "ELCustomerOrderTableViewController.h"
+#import "ELExistingLineItemTableViewCell.h"
+#import "ELExistingOrder.h"
+#import "ELExistingOrderViewController.h"
+#import "ELImageViewController.h"
+#import "ELLineItem.h"
+#import "ELLineItemTableViewCell.h"
+#import "ELLoginViewController.h"
+#import "ELOrder.h"
+#import "ELOrderSummarView.h"
+#import "ELOrderViewController.h"
+#import "ELPaymentMethodEditViewController.h"
+#import "ELPaymentMethodsViewController.h"
+#import "ELPaymentViewController.h"
+#import "ELPickerView.h"
+#import "ELProductAlertView.h"
+#import "ELProductListViewController.h"
+#import "ELProductTableViewCell.h"
+#import "ELProductViewController.h"
+#import "ELPTKView.h"
+#import "ELPumpTableViewCell.h"
+#import "ELTableViewController.h"
+#import "ELTrackingViewController.h"
+#import "ELUserManager.h"
+#import "ELVerifyPasswordView.h"
+
+#import "UIButton+Addons.h"
+#import "UILabel+addOns.h"
+#import "UINavigationController+addOns.h"
+#import "UITableViewCell+addOns.h"
+#import "UIView+UIView_Utilities.h"
+#import "PTKTextField.h"
+
+
+#endif
